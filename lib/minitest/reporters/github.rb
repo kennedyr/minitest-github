dc47906f6ac3a946d5ef407ba5f1a31134007a33
Eric Kennedy <ekennedy50@gmail.com>
2021-01-26 22:09:59 -0500

    init

---
+++ lib/minitest/reporters/github.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "github/version"
+
+module Minitest
+  module Reporters
+    module Github
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
